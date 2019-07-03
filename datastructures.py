1b203f251183f870a37fd457e3d50942e3e18bb2
Mikaeil Orfanian <mokt@outlook.com>
2019-07-03 16:42:09 +0200

    don't duplicate the rendering of md files
    put datastructures in a separate module

---
+++ datastructures.py
@@ -0,0 +1,98 @@
+import copy
+from dataclasses import dataclass
+from datetime import datetime
+import glob
+from typing import List
+
+import markdown
+
+
+@dataclass
+class NavbarItem:
+    title: str
+    link: str = '#'
+
+
+@dataclass
+class ArticleSummary:
+    title: str
+    description: str
+    link: str
+    category: str
+    publication_date: str
+    source_file: str
+    popularity: int = 0
+    root: str = None
+
+    def __post_init__(self):
+        if self.root:
+            self.link = self.root + '/' + self.category + '/' + self.link
+        else:
+            self.link = self.category + '/' + self.link
+        self.publication_date = datetime.strptime(self.publication_date, '%d-%m-%Y')
+
+
+@dataclass
+class HomeCard:
+    title: str
+    articles: List[ArticleSummary]
+
+
+@dataclass
+class SingleArticle:
+    title: str
+    body: str
+
+
+class Articles:
+
+    def __init__(self, path_to_search: str):
+        self.root_path = path_to_search
+        self.markdown_files = self.find_markdown_files()
+        self.metadata = dict()
+        self.summaries = list()
+
+    def find_markdown_files(self):
+        return [f for f in glob.glob(f'{self.root_path}/*.md')]
+
+    def render_markdown_files(self):
+        for in_file in self.markdown_files:
+            extensions = ['codehilite', 'meta']
+            kwargs = dict(
+                input=in_file, 
+                output=in_file.split('.md')[0]+'.html', 
+                extensions=extensions, 
+                encoding='utf-8',
+            )
+            md = markdown.Markdown(**kwargs)
+            md.convertFile(kwargs.get('input', None),
+                        kwargs.get('output', None),
+                        kwargs.get('encoding', None))
+
+            self.metadata[in_file] = md.Meta
+
+            self.summaries.append(ArticleSummary(
+                title=md.Meta['title'][0], 
+                description=md.Meta['description'][0],
+                link=md.Meta['page_name'][0],
+                category=md.Meta['category'][0],
+                publication_date=md.Meta['publication_date'][0],
+                popularity=md.Meta['popularity'][0],
+                source_file=in_file,
+            ))
+
+    def get_top_articles_by_attribute_and_category(self, attribute: str, category: str, top_x: int=None):
+        summaries = copy.deepcopy(self.summaries)
+        articles_in_category = [article for article in summaries if article.category == category]
+
+        if attribute == 'publication_date':
+            articles_in_category.sort(key=lambda article: article.publication_date, reverse=True)
+        elif attribute == 'popularity':
+            articles_in_category.sort(key=lambda article: article.popularity, reverse=True)
+        else:
+            raise ValueError(f'Sorting by {attribute} not supported!')
+
+        if top_x:
+            return articles_in_category[:top_x]
+        else:
+            return articles_in_category
--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import codecs
-from dataclasses import dataclass
-from datetime import datetime
-import glob
-from typing import List
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import markdown
+
+from datastructures import Articles, ArticleSummary, HomeCard, NavbarItem, SingleArticle
 
 
 env = Environment(
@@ -14,34 +12,11 @@ env = Environment(
 )
 
 
-@dataclass
-class NavbarItem:
-    title: str
-    link: str = '#'
-
-
-@dataclass
-class ArticleSummary:
-    title: str
-    description: str
-    link: str
-    category: str
-    publication_date: str
-    popularity: int = 0
-    root: str = None
-
-    def __post_init__(self):
-        if self.root:
-            self.link = self.root + '/' + self.category + '/' + self.link
-        else:
-            self.link = self.category + '/' + self.link
-        self.publication_date = datetime.strptime(self.publication_date, '%d-%m-%Y')
-
-
-@dataclass
-class HomeCard:
-    title: str
-    articles: List[ArticleSummary]
+def find_latest_article():
+    articles = Articles('articles')
+    articles.render_markdown_files()
+    return articles.get_top_articles_by_attribute_and_category('publication_date', 'technical')
+find_latest_article()
 
 
 def generate_index_page():
@@ -58,6 +33,7 @@ def generate_index_page():
         link='types-versus-classes.html',
         category='technical',
         publication_date='02-07-2019',
+        source_file='self-driving-vehicle.md',
     )
     coder_card = HomeCard(title='Top 1% coder', articles=[article1])
     
@@ -69,7 +45,7 @@ def generate_index_page():
     )
     with open('index.html', 'w') as f:
         f.write(rendered_tempalte)
-generate_index_page()
+# generate_index_page()
 
 
 def generate_technical_articles_page():
@@ -87,14 +63,15 @@ def generate_technical_articles_page():
         link='types-versus-classes.html',
         category='technical',
         publication_date='02-07-2019',
+        source_file='self-driving-vehicle.md',
     )
-    latest_article = article1
+    featured_article = article1
 
     technical_articles_template = env.get_template('technical-articles-template.html')
     rendered_tempalte = technical_articles_template.render(
         navbar_items=article_category_navbar_items, 
         technical_articles=[article1],
-        latest_article=latest_article,
+        featured_article=featured_article,
         header_link='../index.html',
     )
     with open('pages/technical-articles.html', 'w') as f:
@@ -102,23 +79,17 @@ def generate_technical_articles_page():
 generate_technical_articles_page()
 
 
-@dataclass
-class Article:
-    title: str
-    body: str
-
-
 def generate_article():
-    in_file = 'articles/self-driving-vehicle.md'
-    out_file = 'articles/self-driving-vehicle-out.html'
-    extensions = ['codehilite', 'meta']
-    # markdown.markdownFromFile(input=in_file, extensions=extensions, output=out_file, encoding='utf-8')
-    kwargs = dict(input=in_file, extensions=extensions, output=out_file, encoding='utf-8')
+    # in_file = 'articles/self-driving-vehicle.md'
+    out_file = 'articles/self-driving-vehicle.html'
+    # extensions = ['codehilite', 'meta']
+    # # markdown.markdownFromFile(input=in_file, extensions=extensions, output=out_file, encoding='utf-8')
+    # kwargs = dict(input=in_file, extensions=extensions, output=out_file, encoding='utf-8')
 
-    md = markdown.Markdown(**kwargs)
-    md.convertFile(kwargs.get('input', None),
-                   kwargs.get('output', None),
-                   kwargs.get('encoding', None))
+    # md = markdown.Markdown(**kwargs)
+    # md.convertFile(kwargs.get('input', None),
+    #                kwargs.get('output', None),
+    #                kwargs.get('encoding', None))
 
     out_file_handle = codecs.open(out_file, mode='r', encoding='utf-8')
     body = out_file_handle.read()
@@ -129,7 +100,7 @@ def generate_article():
     employment = NavbarItem(title='Employment')
     article_navbar_items = (intro, technical, employment)
     
-    article = Article(title='Types versus Classes', body=body)
+    article = SingleArticle(title='Types versus Classes', body=body)
 
     article_template = env.get_template('article-template.html')
     rendered_tempalte = article_template.render(
@@ -142,59 +113,8 @@ def generate_article():
 generate_article()
 
 
-def find_article_files():
-    return [f for f in glob.glob('articles/*.md')]
-
-
-def render_articles_and_get_their_metadata(filenames):
-    metadata = []
-
-    for in_file in filenames:
-        in_file = 'articles/self-driving-vehicle.md'
-        extensions = ['codehilite', 'meta']
-        kwargs = dict(input=in_file, output=in_file.split('.md')[0]+'.html', extensions=extensions, encoding='utf-8')
-        md = markdown.Markdown(**kwargs)
-        md.convertFile(kwargs.get('input', None),
-                    kwargs.get('output', None),
-                    kwargs.get('encoding', None))
-        metadata.append(md.Meta)
-
-    return metadata
-
-
-def make_article_summary_obj_from_article_metadata(meta_data):
-    return ArticleSummary(
-        title=meta_data['title'][0], 
-        description=meta_data['description'][0],
-        link=meta_data['page_name'][0],
-        category=meta_data['category'][0],
-        publication_date=meta_data['publication_date'][0],
-        popularity=meta_data['popularity'][0],
-    )
-
-def find_latest_article():
-    md_files  = find_article_files()
-    metadata = render_articles_and_get_their_metadata(md_files)
-    articles = [
-        make_article_summary_obj_from_article_metadata(mtdata)
-        for mtdata in metadata
-    ]
-    articles.sort(key=lambda article: article.publication_date, reverse=True)
-find_latest_article()
-
-
 def find_popular_articles(top_x: int, category: str=None):
-    md_files  = find_article_files()
-    metadata = render_articles_and_get_their_metadata(md_files)
-    articles = [
-        make_article_summary_obj_from_article_metadata(mtdata)
-        for mtdata in metadata
-    ]
-
-    if category:
-        articles = [article for article in articles if article.category == category]
-
-    articles.sort(key=lambda article: article.popularity, reverse=True)
-    
-    return articles[:top_x]
+    articles = Articles('articles')
+    articles.render_markdown_files()
+    return articles.get_top_articles_by_attribute_and_category('popularity', category, top_x)
 popular_articles = find_popular_articles(2, 'technical')
